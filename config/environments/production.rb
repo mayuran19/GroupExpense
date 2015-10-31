289df952f736e022c83b71b523301dc2b8860ea2
mayuran19 <mayuran19@gmail.com>
2015-11-01 02:52:37 +0800

    bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -84,9 +84,9 @@ Rails.application.configure do
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :doamin               => "gmail.com",
-      :user_name            => "mail.groupexpense@gmail.com",
+      :user_name            => "email.groupexpense@gmail.com",
       :password             => "mayuran123",
-      :authentication       => "login",
+      :authentication       => "plain",
       :enable_starttls_auto => true
   }
 end
